5d5a9734128b05d726266ee52eed790c5fac319c
ZondArt <artemiy.08.2002@mail.ru>
2022-03-25 15:03:55 +0300

    Some change in generation

--- Assets/Dungeon/Generation/SupportScripts/RoomPrefabContainer.cs
+++ Assets/Dungeon/Generation/SupportScripts/RoomPrefabContainer.cs
@@ -6,9 +6,9 @@ using UnityEngine.Tilemaps;
 [CreateAssetMenu(fileName = "RoomPrefabContainer", menuName = "ScriptableObjects/RoomPrefabs/RoomPrefabContainer", order = 0)]
 public class RoomPrefabContainer : ScriptableObject
 {
-    public List<StartRoomPrefab> StartRooms;
-    public List<EnemyRoomPrefab> EnemyRooms;
-    public List<SpecialRoomPrefab> SpecialRooms;
-    public List<DoubeRoomPrefab> DoubeRooms;
-    public List<HallwayPrefab> Hallways;
+    public List<RoomPrefab> StartRooms;
+    public List<RoomPrefab> EnemyRooms;
+    public List<RoomPrefab> SpecialRooms;
+    public List<RoomPrefab> DoubeRooms;
+    public List<RoomPrefab> Hallways;
 }
